e23ab7f5de4c7df55efedc0c94dfd2e36ed8db60
Rob <rob.bailey3@gmail.com>
2019-03-29 16:49:35 +0000

    Adding in loaders for HTML & CSS for web-projects

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,54 @@
-const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require( 'path' );
+const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const WebpackCleanPlugin = require( 'webpack-clean-plugin' );
+const CopyPlugin = require( 'copy-webpack-plugin' );
 
 module.exports = {
   entry: './src/index.ts',
   devtool: 'inline-source-map',
   mode: 'production',
   module: {
-    rules: [
-      {
+    rules: [ {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader'
       }
     ]
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve( __dirname, 'dist' )
   },
   optimization: {
-    minimizer: [new UglifyJsPlugin()]
-  }
+    minimizer: [ new UglifyJsPlugin() ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin( {
+      template: 'src/index.html',
+      filename: 'index.html',
+      chunksSortMode: 'auto',
+      chunks: [ 'main' ]
+    } ),
+    new WebpackCleanPlugin( {
+      on: "emit",
+      path: [ './dist' ]
+    } ),
+    new CopyPlugin( [ {
+      from: 'src/assets',
+      to: 'assets'
+    } ] ),
+  ]
 };
